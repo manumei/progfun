6fe8191a1b977436f4e62b1dea8f25055abd313b
manumei <supremedragonwarrior@gmail.com>
2026-09-11 11:50:16 +0200

    paper

---
+++ lab1/paper.c
@@ -0,0 +1,36 @@
+/* file : paper.c */
+/* author : Manuel Meiriño (m.meirino.ferro@student.rug.nl) */
+/* date : 11-09-2026 */
+/* version: 1.0 */
+/* compile: gcc -std=c99 -Wall -pedantic paper.c */
+
+/* Description:
+    Calculates number of packs needed for exam papers
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  // declare
+  int exams_num;
+  int sheets_per_exam;
+  int pages_per_pack;
+
+  // read input
+  scanf("%d %d %d", &exams_num, &sheets_per_exam, &pages_per_pack);
+
+  // variables
+  int pages_total = exams_num * sheets_per_exam;
+  int packs_needed = 0;
+
+  // calc
+  if (pages_per_pack > 0) {
+    packs_needed = (pages_total + pages_per_pack - 1) /
+                   pages_per_pack; // Ceiling [a, b] is [a + b - 1] // b
+  }
+
+  // end
+  printf("%d\n", packs_needed);
+  return 0;
+}
